f9004f48d88db1f114b312271f4477f2c5089975
metalels <metalels86@gmail.com>
2018-07-18 14:31:24 +0900

    Update base active_record version.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,11 @@
 require 'simplecov'
-require "codeclimate-test-reporter"
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
 ])
 SimpleCov.start do
-      add_filter "/vendor/"
-      add_filter "/spec/"
+  add_filter "/vendor/"
+  add_filter "/spec/"
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
